7fd2df9f5adf986291204075b522ca1f8f6a9130
hattiephillips <harriet-phillips@hotmail.co.uk>
2018-08-29 18:24:55 -0400

    cross origin request test

--- serverold.js
+++ serverold.js
@@ -1,13 +1,6 @@
 // express is the server that forms part of the nodejs program
 var express = require('express');
 var app = express();
-
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Headers", "X-Requested-With");
-next();
-});
-
 // serve static files - e.g. html, css
 app.use(express.static(__dirname));
 
