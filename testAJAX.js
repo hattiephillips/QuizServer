56f64201dcf5a2c684bc24037fdf60ccdf1ce48a
hattiephillips <harriet-phillips@hotmail.co.uk>
2018-08-29 18:21:37 -0400

    AJAX.html server test

--- testAJAX.js
+++ testAJAX.js
@@ -20,3 +20,14 @@ if (xhr.status == 200 && xhr.status < 300)
 document.getElementById('div1').innerHTML = xhr.responseText;
 }
 }
+xhr = new XMLHttpRequest();
+var filename = document.getElementById("filename").value;
+xhr.open("GET", filename, true);
+xhr.onreadystatechange = processDivChange;
+try {
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+}
+catch (e) {
+// this only works in internet explorer
+}
+xhr.send();
