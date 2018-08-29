0ad2c5ea2cb2696ce5abe9781212644e48fb3ac2
hattiephillips <harriet-phillips@hotmail.co.uk>
2018-08-29 18:06:15 -0400

    sevrver development

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toS
 var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(4443);
-app.get('/test.html', function (req, res) {
+app.get('/new.html', function (req, res) {
 // run some server-side code
 console.log('new.html requested');
 // note that __dirname gives the path to the server.js file
