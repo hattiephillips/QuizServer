62109425602e437e86b648eb5ded7c23f9a08963
hattiephillips <harriet-phillips@hotmail.co.uk>
2018-08-29 17:34:30 -0400

    adding server, needs connection details, current basic overview

--- testserver3.js
+++ testserver3.js
@@ -1,3 +1,23 @@
+// express is the server that forms part of the nodejs program
+var express = require('express');
+var app = express();
+
+//Add the body-parser to httpServer.js so that you will be able to process the uploaded data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+app.use(bodyParser.json());
+
+//Add the following code to actually do the POST request to server.js and upload this to Ubuntu and GitHub (note the use of POST this time!)
+app.post('/uploadData',function(req,res){
+// note that we are using POST here as we are uploading data
+// so the parameters form part of the BODY of the request rather than the RESTful API
+console.dir(req.body);
+// for now, just echo the request back to the client
+res.send(req.body);
+});
+
 //Import the required connectivity code and set up a database connection
 var pg = require('pg');
 var pool = new pg.Pool(config);
